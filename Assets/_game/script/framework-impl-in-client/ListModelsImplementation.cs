a18b3f953d798505be3454f1fd75b03d22d36afb
𝔑𝔞𝔪 𝔑𝔤𝔲𝔶𝔢𝔫 <nguyenhoainam@hoppergames.io>
2026-03-30 16:36:16 +0700

    add model framework

---
+++ Assets/_game/script/framework-impl-in-client/ListModelsImplementation.cs
@@ -0,0 +1,10 @@
+
+using System.Collections.Generic;
+
+public class ListModelsImplementation : IListModelDeclaration
+{
+    public List<BasePlayerModel> listModels => new List<BasePlayerModel>()
+    {
+        new CurrencyModel(),
+    };
+}
