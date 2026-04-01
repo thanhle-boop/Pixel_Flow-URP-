7e6eba9413a3ef75ed3a7e5df86ffefe5c516011
quangsy <nguyensy@hoppergames.io>
2026-04-01 13:31:32 +0700

    add anim arrow for conveyor

---
+++ Assets/_game/script/gameplay/conveyorbelt/ConveyorbeltArrow.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ConveyorbeltArrow : MonoBehaviour
+{
+    [SerializeField] LineRenderer lineRenderer;
+    [SerializeField] float speedAnim = 1;
+
+    bool isPause = false;   
+    float valueCore = 0;
+
+    private void Update()
+    {
+        if(isPause) return;
+
+        valueCore -= speedAnim * Time.deltaTime;
+        lineRenderer.material.SetFloat("_TextureCoreModify_X", valueCore);
+    }
+
+    public void SetPauseAnimConveyorbelt(bool val)
+    {
+        isPause = val;
+    }    
+}
