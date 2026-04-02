378dacf5044badef83d7c1d1656fd74238bef111
𝔑𝔞𝔪 𝔑𝔤𝔲𝔶𝔢𝔫 <nguyenhoainam@hoppergames.io>
2026-04-02 14:03:13 +0700

    no message

---
+++ Assets/_game/script/framework-impl-in-client/MapLevelConfigIO.cs
@@ -0,0 +1,19 @@
+
+// using System.IO;
+
+// public class MapLevelConfigIO : ExtraConfigReadWriteManager
+// {
+//     public override string configFilename => "map_level_cfg";
+
+//     protected override void OnReadConfig_binary(BinaryReader reader)
+//     {
+//     }
+
+//     protected override void OnReadConfig_text(string text)
+//     {
+//     }
+
+//     protected override void OnWriteConfig_binary(BinaryWriter writer)
+//     {
+//     }
+// }
