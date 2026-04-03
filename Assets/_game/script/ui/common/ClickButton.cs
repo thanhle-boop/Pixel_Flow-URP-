10374eebbc6ac4cbd0d8817130831b5b25023192
thanhle-boop <thanhle@hoppergames.io>
2026-04-03 11:54:20 +0700

    update button anim

---
+++ Assets/_game/script/ui/common/ClickButton.cs
@@ -0,0 +1,31 @@
+using DG.Tweening;
+using R3;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ClickButton : MonoBehaviour
+{
+    [SerializeField] Button btn;
+
+    void Reset()
+    {
+        btn = GetComponent<Button>();
+    }
+
+    void Start()
+    {
+        btn.OnClickAsObservable().Subscribe(_ =>
+        {
+            RunAnimClick();
+        }).AddTo(this);
+    }
+
+    void RunAnimClick()
+    {
+        btn.transform.DOScale(1.1f, 0.1f).OnComplete(() =>
+        {
+            btn.transform.DOScale(1f, 0.1f);
+        });
+    }
+}
